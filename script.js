9a60520e27c48f2a251dc452ca386bafc9c03191
1sauravm <145886724+1sauravm@users.noreply.github.com>
2023-12-01 04:35:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
-function changeVolume(amount) {
-    let volume = document.getElementById('volume');
-    let newVolume = parseInt(volume.innerText) + amount;
-    volume.innerText = Math.max(0, Math.min(10, newVolume)); // Limit between 0 and 10
+function navigate(page) {
+    // Hide all pages
+    var pages = document.getElementsByClassName('page');
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none';
+    }
+
+    // Show the selected page
+    document.getElementById(page).style.display = 'block';
 }
 
-function changeTemperature(side, amount) {
-    let tempElement = document.getElementById(side + 'Temp');
-    let newTemp = parseInt(tempElement.innerText) + amount;
-    tempElement.innerText = newTemp + '°C';
-}
-
-// Example of handling music source selection
-document.getElementById('radio').addEventListener('click', function() {
-    alert('Radio selected');
-});
-// Similarly, add event listeners for 'phone' and 'cd'
-
-// Lock and Unlock Vehicle
-document.getElementById('lock').addEventListener('click', function() {
-    alert('Vehicle Locked');
-});
-
-document.getElementById('unlock').addEventListener('click', function() {
-    alert('Vehicle Unlocked');
-});
-
-// Open Trunk
-document.getElementById('trunk').addEventListener('click', function() {
-    alert('Trunk Opened');
-});
+// Initial navigation to the home page
+navigate('home');
